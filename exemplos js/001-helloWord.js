a027ab97c6adc1b793a155f5b963c4d5f22f1b46
kelisilva <125319180+kelisilva@users.noreply.github.com>
2023-02-13 22:50:24 -0300

    aula introdução
    criação de variaveis  concatenação e tipagem

---
+++ exemplos js/001-helloWord.js
@@ -0,0 +1,40 @@
+
+/// comentario de uma linha
+console.log('Estou desenvolvendo em java-script');
+
+/*
+comentario de mais de 
+uma linha
+tres formas de declarar uma variavel em java-script
+(var,let e const)
+*/
+
+let nome ='Keli';
+let idade = 38;
+let trabalha = true ;
+let altura_pessoa = 1.60 ;
+let alturaPessoa = 1.60 ;// nº entre aspas é um string
+let coresFavoritas = ['roxo','preto','verde'];//  array
+let endereco = {    
+    logradouro :'Avenida',
+    nmLog : 'Brasil',
+    num :'20',
+    bairro:'Centro'
+};// objeto
+let covid  = null;
+let dengue;
+console.log (altura_pessoa);
+console.log ('nome : ' + nome);
+console.log ('0'  + nome + 'tem' + idade + 'anos.');
+console.log('');;
+console.log('------------------')
+console.log(typeof(trabalha))
+
+
+
+
+
+
+
+ 
+
